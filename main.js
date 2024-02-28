5f555279d61dbbd45661da36d014a7459e4e6910
ammar <ammarkhan575@gmail.com>
2024-02-29 02:55:46 +0530

    investment completed

---
+++ main.js
@@ -0,0 +1,28 @@
+const chart = document.querySelector('#chart').getContext('2d');
+
+
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+            'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+        datasets: [
+            {
+                label: 'BTC',
+                data: [29374, 33474, 28382, 34783, 81345, 12899, 23323, 25678, 33765, 19001, 22098],
+                borderColor: 'red',
+                borderWidth: 2
+            },
+            {
+                label: 'ETH',
+                data: [33374, 12474, 19382, 31113, 21345, 19009, 16323, 29678, 35165, 37001, 30098],
+                borderColor: 'blue',
+                borderWidth: 2
+            }
+
+        ]
+    },
+    options: {
+        responsive: true
+    }
+})
